546880f3e9a440e728debbd9e816e79bdff7d927
Deniz <mdcengiz@gmail.com>
2020-05-18 16:34:24 -0500

    Database setup and saving data to Firestore

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import Firebase
 
 class ChatViewController: UIViewController {
-
+    
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
+    
+    let db = Firestore.firestore()
     
     var messages: [Message] = [
         Message(sender: "1@2.com", body: "Hey!"),
@@ -26,19 +28,32 @@ class ChatViewController: UIViewController {
         tableView.dataSource = self
         title = K.appName
         navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messagebody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            db.collection(K.FStore.collectionName).addDocument(data: [K.FStore.senderField: messageSender,
+                                                                      K.FStore.bodyField: messagebody])
+            {(error) in
+                if let e = error {
+                    print("There was an issue saving data to firestore, \(e)")
+                } else {
+                    print("Successfully saved data.")
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
         do {
-          try Auth.auth().signOut()
+            try Auth.auth().signOut()
             navigationController?.popToRootViewController(animated: true)
         } catch let signOutError as NSError {
-          print ("Error signing out: %@", signOutError)
+            print ("Error signing out: %@", signOutError)
         }
-          
+        
     }
     
 }
@@ -49,8 +64,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.label?.text = messages[indexPath.row].body
         return cell
     }
     
---
+++ Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,28 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Mehmet Deniz Cengiz on 5/18/20.
+//  Copyright © 2020 Deniz Cengiz. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    @IBOutlet var messageBubble: UIView!
+    @IBOutlet var label: UILabel!
+    @IBOutlet var rightImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        messageBubble.layer.cornerRadius = messageBubble.frame.size.height / 5
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
